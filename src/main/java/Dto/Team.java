2d41712faa375f023c3a2776b5e7b0e1bc977d72
fightwing <18071766136@163.com>
2020-10-27 20:18:54 +0800

    add TeamService

--- src/main/java/Dto/Team.java
+++ src/main/java/Dto/Team.java
@@ -6,4 +6,25 @@ public class Team {
 
     private String name;
     private List<Member> members;
+
+    public Team(String name, List<Member> members) {
+        this.name = name;
+        this.members = members;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
 }
--- src/main/java/service/TeamService.java
+++ src/main/java/service/TeamService.java
@@ -1,19 +1,29 @@
 package service;
 
+import Dto.Member;
 import Dto.Team;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class TeamService {
 
+    public static final int NUMBER_OF_TEAMS = 6;
+
     public static final List<Team> teams = new ArrayList<>();
 
-    public List<Team> getTeams(){
+    public List<Team> divideIntoTeams(List<Member> members){
+        Random random = new Random();
+        for (int i = 0;i<members.size();i++){
+            List<Member> mockMembers = members;
+            int randomId = random.nextInt(mockMembers.size());
+            teams.get(i%NUMBER_OF_TEAMS).getMembers().add(mockMembers.get(randomId));
+            mockMembers.remove(randomId);
+        }
         return teams;
     }
-
 
 }
