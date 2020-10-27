3cc33b11b45317062d629c3aa7b6ecf8387fc7a5
fightwing <18071766136@163.com>
2020-10-27 20:27:42 +0800

    add MemberController

--- src/main/java/controller/MemberController.java
+++ src/main/java/controller/MemberController.java
@@ -1,8 +1,27 @@
 package controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import Dto.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.MemberService;
 
+import java.util.List;
+
+@CrossOrigin
 @RestController
 public class MemberController {
 
+    @Autowired
+    MemberService memberService;
+
+    @PostMapping("/member")
+    public ResponseEntity<List<Member>> addMember(@RequestBody Member member){
+        return ResponseEntity.ok(memberService.addMember(member));
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity<List<Member>> getMembers(){
+        return ResponseEntity.ok(memberService.getMembers());
+    }
 }
