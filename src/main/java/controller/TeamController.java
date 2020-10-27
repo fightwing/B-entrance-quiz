4eead3131e33ad79c553b78db16772ff7ee90a84
fightwing <18071766136@163.com>
2020-10-27 20:31:01 +0800

    add TeamController

--- src/main/java/controller/TeamController.java
+++ src/main/java/controller/TeamController.java
@@ -1,17 +1,26 @@
 package controller;
 
 import Dto.Team;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import service.MemberService;
+import service.TeamService;
 
 import java.util.List;
 
 public class TeamController {
 
+    @Autowired
+    TeamService teamService;
+
+    @Autowired
+    MemberService memberService;
+
     @GetMapping("/list")
     public ResponseEntity<List<Team>> teamList(){
-        return ResponseEntity.ok(null);
+        return ResponseEntity.ok(teamService.divideIntoTeams(memberService.getMembers()));
     }
 }
