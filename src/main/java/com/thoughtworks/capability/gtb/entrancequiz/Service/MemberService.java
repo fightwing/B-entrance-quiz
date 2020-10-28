0817f2f077ec74a8c92501551ccb762837562ad6
fightwing <18071766136@163.com>
2020-10-28 13:36:42 +0800

    add initMembers

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/Service/MemberService.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/Service/MemberService.java
@@ -13,6 +13,26 @@ public class MemberService {
 
     public static  int START_ID = 1;
 
+    public List<Member> initMembers(){
+        List<Member> members = new ArrayList<>();
+        members.add(new Member(1,"成吉思汗"));
+        members.add(new Member(1,"鲁班七号"));
+        members.add(new Member(1,"太乙真人"));
+        members.add(new Member(1,"钟无艳"));
+        members.add(new Member(1,"花木兰"));
+        members.add(new Member(1,"雅典娜"));
+        members.add(new Member(1,"芈月"));
+        members.add(new Member(1,"白起"));
+        members.add(new Member(1,"刘禅"));
+        members.add(new Member(1,"庄周"));
+        members.add(new Member(1,"马超"));
+        members.add(new Member(1,"刘备"));
+        members.add(new Member(1,"哪吒"));
+        members.add(new Member(1,"大乔"));
+        members.add(new Member(1,"蔡文姬"));
+        return members;
+    }
+
     public List<Member> addMember(Member member){
         member.setId(START_ID);
         START_ID++;
@@ -21,6 +41,6 @@ public class MemberService {
     }
 
     public List<Member> getMembers(){
-        return members;
+        return initMembers();
     }
 }
